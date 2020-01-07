57ab7b73418b9310340ade0e4dbf41f57d424d87
Jackie <JGtheophila19@gmail.com>
2020-01-07 12:28:19 -0500

    if/else with bool and float

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -1,27 +1,21 @@
 using System;
 
 namespace Conditionals
-{
+{	
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			int hour = 10;
+			bool isGoldCustumer = true;
 			
-			if (hour > 0 && hour < 12)
-			{
-				Console.WriteLine("It's morning.");
-			}
+			float price;
 			
-			else if (hour >= 12 && hour < 18)
-			{
-				Console.WriteLine("It's afternoon.");
-			}
+			if (isGoldCustumer)
+			price = 19.95f
 			
 			else
-			{
-				Console.WriteLine("It's evening.");
-			}
+			price 29.95f;
+			
+			float price = (isGoldCustumer) ? 19.95f : 29.95f;
 		}
-	}
 }
