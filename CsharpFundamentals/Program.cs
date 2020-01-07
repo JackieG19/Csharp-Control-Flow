72808ce6727a7ae72a3ccdf92e9a979c64477758
Jackie <JGtheophila19@gmail.com>
2020-01-07 12:24:12 -0500

    if/else  and else if statement

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-/*using statement*/
+using System;
 
-namespace HelloWorld
+namespace Conditionals
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int hour = 10;
+			
+			if (hour > 0 && hour < 12)
+			{
+				Console.WriteLine("It's morning.");
+			}
+			
+			else if (hour >= 12 && hour < 18)
+			{
+				Console.WriteLine("It's afternoon.");
+			}
+			
+			else
+			{
+				Console.WriteLine("It's evening.");
+			}
+		}
+	}
 }
